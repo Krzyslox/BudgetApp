73846a8534d6893f157497c5797a99f64f7cd53f
Chris <krzysiek9k@gmail.com>
2020-06-24 21:55:10 +0200

    Added constructors for Expanses and Incomes

--- app.js
+++ app.js
@@ -2,20 +2,52 @@
 // BUDGET CONTROLLER
 const budgetController = (() => {
 
+    let Expanse = (id, description, value) => {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    let Income = (id, description, value) => {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    let data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
 
 })();
 
 //UI CONTROLLER
 const UIController = (() => {
 
+    const DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    }
+
     return {
         getInput: () => {
             return {
-                type: document.querySelector('.add__type').value, //Will be inc or exp
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMstrings.inputType).value, //Will be inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
             }
+        },
+        // Przkazanie parametrów DOM do innego modułu, za pośrednictwem upublicznienia
+        getDOMStrings: () => {
+            return DOMstrings;
         }
     }
 
@@ -24,6 +56,21 @@ const UIController = (() => {
 
 //GLOBAL APP CONTROLER
 const controller = ((budgetCtrl, UICtrl) => {
+
+    //Initialization Function
+    let setupEventListeners = () => {
+        const DOM = UICtrl.getDOMStrings();
+
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem)
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        })
+    }
+
+
 
     const ctrlAddItem = () => {
         // 1. Get the field input data
@@ -40,12 +87,14 @@ const controller = ((budgetCtrl, UICtrl) => {
         // 5. Display the budget on UI
 
     }
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
 
-    document.addEventListener('keypress', function (event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: () => {
+            setupEventListeners();
         }
-    })
+    }
+
 
 })(budgetController, UIController)
+
+controller.init();
