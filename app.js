cc7f72fab113a5362cc27ac29205bb72c474a2c5
Chris <krzysiek9k@gmail.com>
2020-06-25 17:10:58 +0200

    Added new method to DOM

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 // BUDGET CONTROLLER
 const budgetController = (() => {
 
-    let Expanse = (id, description, value) => {
+    const Expanse = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
 
-    let Income = (id, description, value) => {
+    const Income = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
@@ -25,6 +25,40 @@ const budgetController = (() => {
         }
     }
 
+    return {
+
+        addItem: (type, des, val) => {
+
+            let newItem;
+            let ID;
+
+            //Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1
+            } else {
+                ID = 0;
+            }
+
+            //Create new item based on inc or exp type
+            if (type === "exp") {
+                newItem = new Expanse(ID, des, val);
+            } else if (type === "inc") {
+                newItem = new Income(ID, des, val);
+            }
+            //new Item added to table
+            data.allItems[type].push(newItem);
+
+            return newItem;
+        },
+        testing: () => {
+            return console.log(data);
+        }
+
+
+
+
+    }
+
 })();
 
 //UI CONTROLLER
@@ -34,7 +68,10 @@ const UIController = (() => {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputButton: '.add__btn'
+        inputButton: '.add__btn',
+        incomeList: '.income__list',
+        epensesList: '.expenses__list'
+
     }
 
     return {
@@ -48,6 +85,26 @@ const UIController = (() => {
         // Przkazanie parametrów DOM do innego modułu, za pośrednictwem upublicznienia
         getDOMStrings: () => {
             return DOMstrings;
+        },
+        addListItem: (obj, type) => {
+            let html, element;
+            // Create HTML String with placeholder text
+            if (type === 'inc') {
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                element = DOMstrings.incomeList;
+            } else if (type === 'exp') {
+                console.log('Thats works');
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                element = DOMstrings.epensesList;
+            }
+            //Replace the placeholder with some actual data
+            let newHTML = html.replace('%id%', obj.id);
+            newHTML = newHTML.replace('%description%', obj.description);
+            newHTML = newHTML.replace('%value%', obj.value);
+
+            // Insert HTML into the DOM
+            return document.querySelector(element).insertAdjacentHTML('beforeend', newHTML);
+
         }
     }
 
@@ -73,14 +130,17 @@ const controller = ((budgetCtrl, UICtrl) => {
 
 
     const ctrlAddItem = () => {
+        let input, newItem;
+
         // 1. Get the field input data
 
-        let input = UICtrl.getInput();
-        console.log(input);
+        input = UICtrl.getInput();
 
         // 2. Add the item to the budget controller
+        newItem = budgetController.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
+        UIController.addListItem(newItem, input.type);
 
         // 4. Calculate the budget
 
